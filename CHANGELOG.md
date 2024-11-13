218133ab4f9d967ce275ff88f2b4ea9d093c2e69
wuhuijian <89465005@qq.com>
2024-11-13 10:36:28 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,35 @@
+## 1.0.7
+
+* package update
+
+## 1.0.6
+
+* package update. 
+* Kotlin version update to 1.9.22
+* Media3 version update to 1.3.1
+* gradle version update to 8.1.4
+* targetSdkVersion update to 34
+
+## 1.0.5
+
+* package update
+
+## 1.0.4
+
+* ios player bug fix
+
+## 1.0.3
+
+* ExoPlayer migration Media3
+
+## 1.0.2
+
+* bug fix
+
+## 1.0.1
+
+* bug fix
+
+## 1.0.0
+
+* Initial release.
---
+++ android/src/main/kotlin/uz/shs/better_player_plus/BetterPlayerCache.kt
@@ -0,0 +1,40 @@
+package uz.shs.better_player_plus
+
+import android.content.Context
+import android.util.Log
+import androidx.media3.database.ExoDatabaseProvider
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
+import java.io.File
+import java.lang.Exception
+
+object BetterPlayerCache {
+    @Volatile
+    private var instance: SimpleCache? = null
+    fun createCache(context: Context, cacheFileSize: Long): SimpleCache? {
+        if (instance == null) {
+            synchronized(BetterPlayerCache::class.java) {
+                if (instance == null) {
+                    instance = SimpleCache(
+                        File(context.cacheDir, "betterPlayerCache"),
+                        LeastRecentlyUsedCacheEvictor(cacheFileSize),
+                        ExoDatabaseProvider(context)
+                    )
+                }
+            }
+        }
+        return instance
+    }
+
+    @JvmStatic
+    fun releaseCache() {
+        try {
+            if (instance != null) {
+                instance!!.release()
+                instance = null
+            }
+        } catch (exception: Exception) {
+            Log.e("BetterPlayerCache", exception.toString())
+        }
+    }
+}
